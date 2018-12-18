64f8587ea0a744ad3dbe3b9a8f619ef2b1033d8c
Rayli Raykov <rayli208@gmail.com>
2018-12-17 19:54:45 -0500

    final copy

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,6 @@
 var goalPoints = Math.floor(Math.random() * 120) + 19; 
-var wins = $("#wins");
-var losses = $("#losses");
+var wins = 0;
+var losses = 0;
 var crystal1 = $("#crystal1");
 var crystal2 = $("#crystal2");
 var crystal3 = $("#crystal3");
@@ -27,27 +27,103 @@ crystal1.click(function() {
   crystalValue = parseInt(crystalValue);
   total += crystalValue;
   $("#total").html(total);
+
+  if(total === goalPoints){
+    alert("You win");
+    wins++;
+    goalPoints = Math.floor(Math.random() * 120) + 19;
+    crystal1.attr("data-crystalvalue", getRandomNumber());
+    $("#goalPoints").html(goalPoints);
+    total = 0;
+    $("#wins").html(wins);
+    $("#total").html(total);
+  }else if(total > goalPoints){
+    alert("You lose");
+    losses++;
+    goalPoints = Math.floor(Math.random() * 120) + 19;
+    crystal1.attr("data-crystalvalue", getRandomNumber());
+    $("#goalPoints").html(goalPoints);
+    total = 0;
+    $("#losses").html(losses);
+    $("#total").html(total);
+  }
 });
 crystal2.click(function() {
   var crystalValue = ($(this).attr("data-crystalvalue"));
   crystalValue = parseInt(crystalValue);
   total += crystalValue;
   $("#total").html(total);
+
+  if(total === goalPoints){
+    alert("You win");
+    wins++;
+    goalPoints = Math.floor(Math.random() * 120) + 19;
+    crystal1.attr("data-crystalvalue", getRandomNumber());
+    $("#goalPoints").html(goalPoints);
+    total = 0;
+    $("#wins").html(wins);
+    $("#total").html(total);
+  }else if(total > goalPoints){
+    alert("You lose");
+    losses++;
+    goalPoints = Math.floor(Math.random() * 120) + 19;
+    crystal1.attr("data-crystalvalue", getRandomNumber());
+    $("#goalPoints").html(goalPoints);
+    total = 0;
+    $("#losses").html(losses);
+    $("#total").html(total);
+  }
 });
 crystal3.click(function() {
   var crystalValue = ($(this).attr("data-crystalvalue"));
   crystalValue = parseInt(crystalValue);
   total += crystalValue;
   $("#total").html(total);
+
+  if(total === goalPoints){
+    alert("You win");
+    wins++;
+    goalPoints = Math.floor(Math.random() * 120) + 19;
+    crystal1.attr("data-crystalvalue", getRandomNumber());
+    $("#goalPoints").html(goalPoints);
+    total = 0;
+    $("#wins").html(wins);
+    $("#total").html(total);
+  }else if(total > goalPoints){
+    alert("You lose");
+    losses++;
+    goalPoints = Math.floor(Math.random() * 120) + 19;
+    crystal1.attr("data-crystalvalue", getRandomNumber());
+    $("#goalPoints").html(goalPoints);
+    total = 0;
+    $("#losses").html(losses);
+    $("#total").html(total);
+  }
 });
 crystal4.click(function() {
   var crystalValue = ($(this).attr("data-crystalvalue"));
   crystalValue = parseInt(crystalValue);
   total += crystalValue;
   $("#total").html(total);
-});
 
-if(total > goalPoints){
-  console.log("You win");
-}
+  if(total === goalPoints){
+    alert("You win");
+    wins++;
+    goalPoints = Math.floor(Math.random() * 120) + 19;
+    crystal1.attr("data-crystalvalue", getRandomNumber());
+    $("#goalPoints").html(goalPoints);
+    total = 0;
+    $("#wins").html(wins);
+    $("#total").html(total);
+  }else if(total > goalPoints){
+    alert("You lose");
+    losses++;
+    goalPoints = Math.floor(Math.random() * 120) + 19;
+    crystal1.attr("data-crystalvalue", getRandomNumber());
+    $("#goalPoints").html(goalPoints);
+    total = 0;
+    $("#losses").html(losses);
+    $("#total").html(total);
+  }
+});
 
